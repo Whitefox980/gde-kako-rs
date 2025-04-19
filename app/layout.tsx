425d2d348393e145a6bd158416f1ece13055c7c2
Whitefox980 <ivannestorov80@gmail.com>
2025-04-19 15:11:00 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="sr">
-      <body>{children}</body>
-    </html>
-  )
-}
+import { GeistSans, GeistMono } from 'geist/font';
+   
+   export default function RootLayout({
+     children,
+   }: {
+     children: React.ReactNode
+   }) {
+     return (
+       <html lang="sr" className={`${GeistSans.variable} ${GeistMono.variable}`}>
+         <body>{children}</body>
+       </html>
+     )
+   }
